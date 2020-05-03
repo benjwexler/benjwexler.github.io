ea0fde9039449606e76b8ebccbb922473f9e19ce
Ben Wexler <mixedywex@gmail.com>
2020-05-03 02:48:40 -0400

    quick fix(commented out)  for preventing horizontal scroll in carousel

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+let touchPositionStartY = 0;
+let currentPositionY;
+
+// document.getElementById('mh-client-review').addEventListener("touchmove", function(ev) {
+//   currentPositionY = ev.touches[0].clientY;
+//   if(Math.abs(touchPositionStartY - currentPositionY) > 50) {
+//     return document.body.style.overflow = 'auto';
+//   }
+//   return document.body.style.overflow = 'hidden';
+//   console.log('touchPositionStartY', touchPositionStartY)
+//   console.log('currentPositionY', currentPositionY)
+// }, {passive: false});
+
+// document.getElementById('mh-client-review').addEventListener("touchstart", function(ev) {
+//   document.body.style.overflow = 'hidden';
+//   touchPositionStartY = ev.touches[0].clientY;
+// }, {passive: false});
+
+// document.getElementById('mh-client-review').addEventListener("touchend", function() {
+
+//   document.body.style.overflow = 'auto';
+// }, {passive: false});
