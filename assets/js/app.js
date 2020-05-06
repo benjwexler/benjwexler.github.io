c1982d8a26bba0dbb66ff651d6feca06f447ab9d
Ben Wexler <mixedywex@gmail.com>
2020-05-05 23:57:51 -0400

    prevent disjointed scrollling for references when swiping on mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,33 @@
 let touchPositionStartY = 0;
+let touchPositionstartX = 0;
 let currentPositionY;
+let currentPositionX;
+function getDegreesFromAtan(_oppOverAdjacent) {
+  return Math.atan(_oppOverAdjacent) * (180/Math.PI) ;
+}
 
-// document.getElementById('mh-client-review').addEventListener("touchmove", function(ev) {
-//   currentPositionY = ev.touches[0].clientY;
-//   if(Math.abs(touchPositionStartY - currentPositionY) > 50) {
-//     return document.body.style.overflow = 'auto';
-//   }
-//   return document.body.style.overflow = 'hidden';
-//   console.log('touchPositionStartY', touchPositionStartY)
-//   console.log('currentPositionY', currentPositionY)
-// }, {passive: false});
+document.getElementById('mh-client-review').addEventListener("touchmove", function(ev) {
+  currentPositionY = ev.touches[0].clientY;
+  currentPositionX = ev.touches[0].clientX;
+  const diffX =  currentPositionX - touchPositionStartX
+  const diffY = currentPositionY - touchPositionStartY 
+  const oppOverAdjacent = diffY/diffX
+  
+  console.log('getTanFromDegrees', getDegreesFromAtan(oppOverAdjacent))
+  if(Math.abs(getDegreesFromAtan(oppOverAdjacent)) >= 45) {
+    
+    return document.body.style.overflow = 'scroll';
+  }
+  ev.preventDefault();
+  return document.body.style.overflow = 'hidden';
+  
+}, );
 
-// document.getElementById('mh-client-review').addEventListener("touchstart", function(ev) {
-//   document.body.style.overflow = 'hidden';
-//   touchPositionStartY = ev.touches[0].clientY;
-// }, {passive: false});
+document.getElementById('mh-client-review').addEventListener("touchstart", function(ev) {
+  touchPositionStartY = ev.touches[0].clientY;
+  touchPositionStartX = ev.touches[0].clientX;
+}, );
 
-// document.getElementById('mh-client-review').addEventListener("touchend", function() {
-
-//   document.body.style.overflow = 'auto';
-// }, {passive: false});
+document.getElementById('mh-client-review').addEventListener("touchend", function() {
+  document.body.style.overflow = 'scroll';
+}, );
