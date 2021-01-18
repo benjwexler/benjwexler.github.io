2c7dcfa8dc8a39f9497ff002c72ebb1f37a85c0f
Ben Wexler <mixedywex@gmail.com>
2021-01-17 20:47:32 -0500

    add newest reference

--- assets/js/app.js
+++ assets/js/app.js
@@ -31,3 +31,23 @@ document.getElementById('mh-client-review').addEventListener("touchstart", funct
 document.getElementById('mh-client-review').addEventListener("touchend", function() {
   document.body.style.overflow = 'scroll';
 }, );
+
+function elementInViewport2(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top < (window.pageYOffset + window.innerHeight) &&
+    left < (window.pageXOffset + window.innerWidth) &&
+    (top + height) > window.pageYOffset &&
+    (left + width) > window.pageXOffset
+  );
+}
